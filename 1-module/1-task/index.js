f63ffddd5badd50eaa70d1613badda18025efb5d
Ihor Khrystiuk <i.khristiuk@creatio.com>
2022-06-09 22:00:19 +0300

    implement the factorial function

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,19 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  if (n < 1) {
+    throw new Error('Number should be equal 0 or greater.');
+  }
+
+  let result = 1;
+  while (n > 1) {
+    result *= n;
+    n--;
+  }
+
+  return result;
 }
+
+console.log(factorial(-1));
