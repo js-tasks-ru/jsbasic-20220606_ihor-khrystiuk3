dd99af7f3b060b1b32abfce1ee4c277ced0ce5de
Ihor Khrystiuk <i.khristiuk@creatio.com>
2022-06-10 20:49:39 +0300

    implement ucFirst function

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length === 0) {
+    return str;
+  }
+  const result = str.charAt(0).toUpperCase() + str.slice(1);
+  console.log('str ', str);
+  return result;
 }
