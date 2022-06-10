a292c285bc80c96df1fc2b3581149a89a519269c
Ihor Khrystiuk <i.khristiuk@creatio.com>
2022-06-10 20:36:10 +0300

    implement the isValid function

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,11 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name === null) {return false;}
+  if (name.indexOf(' ') >= 0 || name.length === 0 || name.length < 4) {
+    return false;
+  }
+  return true;
 }
 
 function sayHello() {
@@ -22,3 +26,5 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
+sayHello();
