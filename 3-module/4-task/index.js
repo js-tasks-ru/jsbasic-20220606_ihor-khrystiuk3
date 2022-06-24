7c66e14a33fe2db71e7a5d0b7dc87181e4a1a60b
Ihor Khrystiuk <inanutshell86@gmail.com>
2022-06-24 20:32:46 +0300

    implement the sumSalary function

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  const filteredUsers = users.filter(user => user.age <= age);
+  const result = filteredUsers.map((user, index, users) => {
+    const template = `${user.name}, ${user.balance}`;
+    if (index + 1 === users.length) {
+      return template;
+    }
+    return `${template}\n`;
+  });
+  return result.join('');
 }
