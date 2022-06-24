da3ddb800c2b83d992c1d002f3d4e505432b4a79
Ihor Khrystiuk <inanutshell86@gmail.com>
2022-06-24 21:43:48 +0300

    implement the makeFriendsList function

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('ul');
+  for (let friend of friends) {
+    let item = document.createElement('li');
+    let text = document.createTextNode(`${friend.firstName} ${friend.lastName}`);
+    item.appendChild(text);
+    list.appendChild(item);
+  }
+  return list;
 }
