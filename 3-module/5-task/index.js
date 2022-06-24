58cfe254e5b408eec6b997df7bdbb6b81207183e
Ihor Khrystiuk <inanutshell86@gmail.com>
2022-06-24 20:43:28 +0300

    implement the getMinMax function

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,6 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str.split(' ').filter(item => !isNaN(parseFloat(item)) && isFinite(item));
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+  return { min, max };
 }
